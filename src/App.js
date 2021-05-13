87d6398513941364ef1fc844fc782cddf7b94c92
addison <factor@DESKTOP-DFEJACF.localdomain>
2021-05-13 05:25:43 -0500

    deploy

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import './App.css';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route, BrowserRouter} from 'react-router-dom';
 import Home from './pages';
 import SigninPage from './pages/signin';
-//import BrowserRouter from 'react-router-dom/BrowserRouter';
+import BrowserRouter from 'react-router-dom/BrowserRouter';
 
 
 function App() {
   return (
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Router>
       <Switch>
           <Route path="/" component={Home} exact/>
           <Route path="/signin" component={SigninPage} exact/>
       </Switch>
     </Router>
+    </BrowserRouter>
   );
 }
 
